84430bc2a16925aff9202134d73a0dea7ac8562e
ChristianSeelemann <cseelemann@gmx.de>
2021-08-03 15:00:17 +0200

    🎨  Basic formatting and styling

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,33 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const mainElement = document.createElement('main');
-
-const headlineElement = document.createElement('h1');
-headlineElement.innerText = 'Rick and Morty Fanapp';
-
-const searchField = document.createElement('input');
-searchField.setAttribute('placeholder', 'Search your Charakter');
-
-mainElement.append(headlineElement, searchField);
+const page = createElement('div', {
+  id: 'page',
+  childElements: [
+    createElement('header', {
+      childElements: [
+        createElement('img', {
+          src: './src/assets/Rick_and_Morty.svg',
+          className: 'logo__img',
+        }),
+        createElement('h2', {
+          innerText: 'Fanpage',
+          className: 'headline__h2',
+        }),
+      ],
+    }),
+    createElement('main', {
+      childElements: [
+        createElement('input', {
+          placeholder: 'Search your Charakter',
+        }),
+      ],
+    }),
+  ],
+});
 
 if (app !== null) {
-  app.append(mainElement);
+  app.append(page);
 }
